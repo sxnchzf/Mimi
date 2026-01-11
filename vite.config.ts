4478b1203555fe5125e48172fc93483ed7027616
sxnchzf <sanchezf.moon@gmail.com>
2026-01-11 18:18:49 -0500

    Refactor Vite config to load environment variables

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,20 @@
-
-import path from 'path'
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '')
+  const env = loadEnv(mode, process.cwd(), '')
+
   return {
     base: '/Mimi/',
     plugins: [react()],
     define: {
-      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env': env
     },
     resolve: {
       alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
-    },
+        '@': path.resolve(__dirname, 'src')
+      }
+    }
   }
 })
